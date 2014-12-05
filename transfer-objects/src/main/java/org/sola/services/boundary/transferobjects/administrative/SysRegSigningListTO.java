b33b4ac12013d7539bb534067b75b01a9c3b3be2
Mariapaola Rizzo <mariapaola.rizzo@gmail.com>
2014-12-05 17:17:32 +0100

    Signing List Machine ticket #33

---
+++ transfer-objects/src/main/java/org/sola/services/boundary/transferobjects/administrative/SysRegSigningListTO.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.sola.services.boundary.transferobjects.administrative;
+
+import java.math.BigDecimal;
+import org.sola.services.common.contracts.AbstractIdTO;
+
+/**
+ *
+ * @author RizzoM
+ */
+public class SysRegSigningListTO extends AbstractIdTO {
+
+    private String nameFirstpart;
+    private String nameLastpart;
+    private String parcel;
+    private String persons;
+
+    
+    public SysRegSigningListTO() {
+        super();
+    }
+
+    
+    public String getNameFirstpart() {
+        return nameFirstpart;
+    }
+
+    public void setNameFirstpart(String nameFirstpart) {
+        this.nameFirstpart = nameFirstpart;
+    }
+
+    public String getNameLastpart() {
+        return nameLastpart;
+    }
+
+    public void setNameLastpart(String nameLastpart) {
+        this.nameLastpart = nameLastpart;
+    }
+
+    public String getParcel() {
+        return parcel;
+    }
+
+    public void setParcel(String parcel) {
+        this.parcel = parcel;
+    }
+
+    public String getPersons() {
+        return persons;
+    }
+
+    public void setPersons(String persons) {
+        this.persons = persons;
+    }
+
+}
