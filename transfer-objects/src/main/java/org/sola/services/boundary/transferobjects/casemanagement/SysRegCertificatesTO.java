5751d08a036bd93b858156e311e4f93db019051a
unknown <rizzom@NRC016.hq.un.fao.org>
2016-09-15 17:09:50 +0200

    premium and stamp duty fields in the systematicaregistrationcertificates view, changeset script, ejb, beans

--- transfer-objects/src/main/java/org/sola/services/boundary/transferobjects/casemanagement/SysRegCertificatesTO.java
+++ transfer-objects/src/main/java/org/sola/services/boundary/transferobjects/casemanagement/SysRegCertificatesTO.java
@@ -38,7 +38,34 @@ public class SysRegCertificatesTO extends AbstractIdTO {
     private String rank;
     private Integer term;
     private BigDecimal rent;
+    private BigDecimal stampDuty;
+    private Integer premiumStateLand;
+    private Integer premiumNonState;
 
+    public BigDecimal getStampDuty() {
+        return stampDuty;
+    }
+
+    public void setStampDuty(BigDecimal stampDuty) {
+        this.stampDuty = stampDuty;
+    }
+
+    public Integer getPremiumStateLand() {
+        return premiumStateLand;
+    }
+
+    public void setPremiumStateLand(Integer premiumStateLand) {
+        this.premiumStateLand = premiumStateLand;
+    }
+
+    public Integer getPremiumNonState() {
+        return premiumNonState;
+    }
+
+    public void setPremiumNonState(Integer premiumNonState) {
+        this.premiumNonState = premiumNonState;
+    }
+    
     public Integer getTerm() {
         return term;
     }
