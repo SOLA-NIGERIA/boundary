ae9961dd16d438c36e2816a9b20bf2f304ac22a2
Alexander Solovov <a_solovov@yahoo.com>
2016-12-07 03:20:18 +0600

    Changes to the claims transfer procedure have been made according to LH ticket #93

--- web-services/src/main/java/org/sola/services/boundary/ws/Cadastre.java
+++ web-services/src/main/java/org/sola/services/boundary/ws/Cadastre.java
@@ -29,6 +29,10 @@
  */
 package org.sola.services.boundary.ws;
 
+import com.vividsolutions.jts.geom.Polygon;
+import com.vividsolutions.jts.io.ParseException;
+import com.vividsolutions.jts.io.WKBReader;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import javax.annotation.Resource;
@@ -37,6 +41,8 @@ import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
 import javax.xml.ws.WebServiceContext;
+import org.sola.common.SOLAException;
+import org.sola.common.messaging.ServiceMessage;
 import org.sola.services.boundary.transferobjects.cadastre.*;
 import org.sola.services.boundary.transferobjects.cadastre.CadastreObjectNodeTO;
 import org.sola.services.boundary.transferobjects.cadastre.CadastreObjectTO;
@@ -51,10 +57,16 @@ import org.sola.services.common.br.ValidationResult;
 import org.sola.services.common.contracts.GenericTranslator;
 import org.sola.services.common.faults.*;
 import org.sola.services.common.webservices.AbstractWebService;
+import org.sola.services.ejb.administrative.businesslogic.AdministrativeEJBLocal;
+import org.sola.services.ejb.administrative.repository.entities.BaUnit;
+import org.sola.services.ejb.administrative.repository.entities.BaUnitArea;
 import org.sola.services.ejb.cadastre.businesslogic.CadastreEJBLocal;
+import org.sola.services.ejb.cadastre.repository.entities.CadastreObject;
+import org.sola.services.ejb.cadastre.repository.entities.CadastreObjectTargetRedefinition;
 import org.sola.services.ejb.cadastre.repository.entities.NewCadastreObjectIdentifier;
 import org.sola.services.ejb.cadastre.repository.entities.SpatialUnit;
 import org.sola.services.ejb.cadastre.repository.entities.SpatialUnitGroup;
+import org.sola.services.ejb.cadastre.repository.entities.SpatialValueArea;
 import org.sola.services.ejb.cadastre.repository.entities.SysRegWorkUnit;
 import org.sola.services.ejb.transaction.businesslogic.TransactionEJBLocal;
 import org.sola.services.ejb.transaction.repository.entities.TransactionBulkOperationSpatial;
@@ -63,7 +75,8 @@ import org.sola.services.ejb.transaction.repository.entities.TransactionCadastre
 import org.sola.services.ejb.transaction.repository.entities.TransactionType;
 
 /**
- * Web Service Boundary class to expose {@linkplain org.sola.services.ejb.cadastre.businesslogic.CadastreEJB}
+ * Web Service Boundary class to expose
+ * {@linkplain org.sola.services.ejb.cadastre.businesslogic.CadastreEJB}
  * methods.
  */
 @WebService(serviceName = "cadastre-service", targetNamespace = ServiceConstants.CADASTRE_WS_NAMESPACE)
@@ -73,6 +86,9 @@ public class Cadastre extends AbstractWebService {
     private CadastreEJBLocal cadastreEJB;
     @EJB
     private TransactionEJBLocal transactionEJB;
+    @EJB
+    private AdministrativeEJBLocal administrativeEjb;
+    
     @Resource
     private WebServiceContext wsContext;
 
@@ -262,9 +278,9 @@ public class Cadastre extends AbstractWebService {
 
             @Override
             public void run() {
-                TransactionCadastreChange targetTransaction =
-                        transactionEJB.getTransactionById(
-                        transactionTO.getId(), TransactionCadastreChange.class);
+                TransactionCadastreChange targetTransaction
+                        = transactionEJB.getTransactionById(
+                                transactionTO.getId(), TransactionCadastreChange.class);
                 TransactionCadastreChange transactionCadastreChange = GenericTranslator.fromTO(
                         transactionTO, TransactionCadastreChange.class, targetTransaction);
                 result[0] = transactionEJB.saveTransaction(
@@ -463,19 +479,99 @@ public class Cadastre extends AbstractWebService {
 
             @Override
             public void run() {
-                TransactionCadastreRedefinition targetTransaction =
-                        transactionEJB.getTransactionById(
-                        transactionTOTmp.getId(), TransactionCadastreRedefinition.class);
-                TransactionCadastreRedefinition transactionCadastreRedefinition =
-                        GenericTranslator.fromTO(
-                        transactionTOTmp, TransactionCadastreRedefinition.class, targetTransaction);
+                TransactionCadastreRedefinition targetTransaction
+                        = transactionEJB.getTransactionById(
+                                transactionTOTmp.getId(), TransactionCadastreRedefinition.class);
+                TransactionCadastreRedefinition transactionCadastreRedefinition
+                        = GenericTranslator.fromTO(
+                                transactionTOTmp, TransactionCadastreRedefinition.class, targetTransaction);
 
                 result[0] = transactionEJB.saveTransaction(transactionCadastreRedefinition,
                         TransactionType.REDEFINE_CADASTRE, languageCodeTmp);
+
+                // Get relevant cadastre objects and update their calculated area LH ticket #93
+                for (CadastreObjectTargetRedefinition coR : transactionCadastreRedefinition.getCadastreObjectTargetList()) {
+                    CadastreObject co = cadastreEJB.getCadastreObject(coR.getCadastreObjectId());
+                    if (co != null && coR.getGeomPolygon() != null) {
+                        WKBReader reader = new WKBReader();
+                        try {
+                            Polygon poly = (Polygon) reader.read(coR.getGeomPolygon());
+                            if (co.getSpatialValueAreaList() == null) {
+                                co.setSpatialValueAreaList(new ArrayList<SpatialValueArea>());
+                            }
+
+                            SpatialValueArea areaToUpdate = null;
+
+                            for (SpatialValueArea area : co.getSpatialValueAreaList()) {
+                                if (area.getTypeCode().equalsIgnoreCase("officialArea")) {
+                                    areaToUpdate = area;
+                                    break;
+                                }
+                            }
+
+                            if (areaToUpdate == null) {
+                                areaToUpdate = new SpatialValueArea();
+                                areaToUpdate.setSpatialUnitId(co.getId());
+                                areaToUpdate.setTypeCode("officialArea");
+                                co.getSpatialValueAreaList().add(areaToUpdate);
+                            }
+
+                            // Round the area 
+                            double roundedArea = roundArea(poly.getArea());
+                            areaToUpdate.setSize(new BigDecimal(roundedArea));
+
+                            cadastreEJB.saveCadastreObject(co);
+                            
+                            // Find ba unut areas where cadastre object is involved
+                            List<BaUnitArea> baUnitAreas = administrativeEjb.getBaUnitAreasByCadastreObject(co.getId());
+                            
+                            if(baUnitAreas != null){
+                                for(BaUnitArea area : baUnitAreas){
+                                    // Look for calculated area
+                                    if(area.getTypeCode().equalsIgnoreCase("calculatedArea")){
+                                        area.setSize(new BigDecimal(roundedArea));
+                                        administrativeEjb.createBaUnitArea(area.getBaUnitId(), area);
+                                    }
+                                }
+                            }
+                        } catch (ParseException ex) {
+                            throw new SOLAException(ServiceMessage.EJB_TRANSACTION_FAILED_TO_CALCULATE_AREA);
+                        }
+                    }
+                }
             }
         });
 
         return (List<ValidationResult>) result[0];
+    }
+
+    private double roundArea(double area) {
+        if (area <= 100) {
+            if (area % 1 <= 0.8) {
+                area = area - (area % 1);
+            } else {
+                area = (area - (area % 1)) + 1;
+            }
+        } else if (area > 100 && area <= 1000) {
+            if (area % 10 <= 8) {
+                area = area - (area % 10);
+            } else {
+                area = (area - (area % 10)) + 10;
+            }
+        } else if (area > 1000 && area <= 10000) {
+            if (area % 100 <= 80) {
+                area = area - (area % 100);
+            } else {
+                area = (area - (area % 100)) + 100;
+            }
+        } else if (area > 10000) {
+            if (area % 1000 <= 800) {
+                area = area - (area % 1000);
+            } else {
+                area = (area - (area % 1000)) + 1000;
+            }
+        }
+        return area;
     }
 
     /**
@@ -501,15 +597,15 @@ public class Cadastre extends AbstractWebService {
             public void run() {
                 result[0] = GenericTranslator.toTO(
                         transactionEJB.getTransactionByServiceId(
-                        serviceIdTmp, false, TransactionCadastreRedefinition.class),
+                                serviceIdTmp, false, TransactionCadastreRedefinition.class),
                         TransactionCadastreRedefinitionTO.class);
             }
         });
 
         return (TransactionCadastreRedefinitionTO) result[0];
     }
-    
-     /**
+
+    /**
      * See {{@linkplain CadastreEJB#getSpatialValueArea(java.lang.String)
      * CadastreEJB.getSpatialValueArea(String colist )}
      *
@@ -536,7 +632,7 @@ public class Cadastre extends AbstractWebService {
         return (SpatialValueAreaTO) result[0];
     }
 
-     /**
+    /**
      * See {{@linkplain CadastreEJB#getNewCadastreObjectIdentifier(byte[], java.lang.String)
      * CadastreEJB.getNewCadastreObjectIdentifier(byte[], java.lang.String)}
      *
@@ -561,7 +657,7 @@ public class Cadastre extends AbstractWebService {
 
         return (NewCadastreObjectIdentifier) result[0];
     }
-    
+
     /**
      * See {@linkplain org.sola.services.ejb.cadastre.businesslogic.CadastreEJB#getSpatialUnitGroupByParts(java.lang.String)
      * CadastreEJB.getCadastreObjectByParts}
@@ -590,7 +686,8 @@ public class Cadastre extends AbstractWebService {
 
         return (List<SpatialUnitGroupTO>) result[0];
     }
-     /**
+
+    /**
      * See {@linkplain org.sola.services.ejb.cadastre.businesslogic.CadastreEJB#getSpatialUnitGroupByParts(java.lang.String)
      * CadastreEJB.getCadastreObjectByParts}
      *
@@ -619,6 +716,7 @@ public class Cadastre extends AbstractWebService {
 
         return (List<SpatialUnitGroupTO>) result[0];
     }
+
     /**
      * See {@linkplain org.sola.services.ejb.cadastre.businesslogic.CadastreEJB#getSpatialUnitGroupByAllParts(java.lang.String)
      * CadastreEJB.getCadastreObjectByAllParts}
@@ -647,22 +745,21 @@ public class Cadastre extends AbstractWebService {
 
         return (List<SpatialUnitGroupTO>) result[0];
     }
-   
 
-     /**
-     * See {{@linkplain CadastreEJB#saveSpatialUnitGroups(List<SpatialUnitGroupTO>, String)
-     * CadastreEJB.saveSpatialUnitGroups(byte[], Integer, Integer)}
-     * 
-     * @param items 
-     * @param languageCode 
-     * 
+    /**
+     * See
+     * {{@linkplain CadastreEJB#saveSpatialUnitGroups(List<SpatialUnitGroupTO>,
+     * String) CadastreEJB.saveSpatialUnitGroups(byte[], Integer, Integer)}
+     *
+     * @param items
+     * @param languageCode
+     *
      * @throws SOLAAccessFault
      * @throws SOLAFault
      * @throws UnhandledFault
      * @throws OptimisticLockingFault
      * @throws SOLAValidationFault
      */
-
     @WebMethod(operationName = "SaveSpatialUnitGroups")
     public void SaveSpatialUnitGroups(
             @WebParam(name = "items") List<SpatialUnitGroupTO> items,
@@ -678,31 +775,30 @@ public class Cadastre extends AbstractWebService {
             @Override
             public void run() {
                 List<String> ids = new ArrayList<String>();
-                for(SpatialUnitGroupTO item: itemsTmp){
+                for (SpatialUnitGroupTO item : itemsTmp) {
                     ids.add(item.getId());
                 }
-                List<SpatialUnitGroup> spatialUnitGroupListToSave =
-                        GenericTranslator.fromTOList(itemsTmp, SpatialUnitGroup.class, 
-                        cadastreEJB.getSpatialUnitGroupsByIds(ids));
+                List<SpatialUnitGroup> spatialUnitGroupListToSave
+                        = GenericTranslator.fromTOList(itemsTmp, SpatialUnitGroup.class,
+                                cadastreEJB.getSpatialUnitGroupsByIds(ids));
                 cadastreEJB.saveSpatialUnitGroups(spatialUnitGroupListToSave, languageCodeTmp);
             }
         });
     }
 
-     /**
+    /**
      * See {{@linkplain CadastreEJB#getSpatialUnitGroups(byte[], Integer, Integer)
      * CadastreEJB.getSpatialUnitGroups(byte[], Integer, Integer)}
-     * 
-     * @param filteringGeometry 
-     * @param hierarchyLevel 
-     * @param srid 
-     * 
+     *
+     * @param filteringGeometry
+     * @param hierarchyLevel
+     * @param srid
+     *
      * @throws SOLAAccessFault
      * @throws SOLAFault
      * @throws UnhandledFault
      */
-
-     @WebMethod(operationName = "GetSpatialUnitGroups")
+    @WebMethod(operationName = "GetSpatialUnitGroups")
     public List<SpatialUnitGroupTO> GetSpatialUnitGroups(
             @WebParam(name = "filteringGeometry") final byte[] filteringGeometry,
             @WebParam(name = "hierarchyLevel") final Integer hierarchyLevel,
@@ -723,8 +819,8 @@ public class Cadastre extends AbstractWebService {
 
         return (List<SpatialUnitGroupTO>) result[0];
     }
-     
-         @WebMethod(operationName = "getCadastreObject")
+
+    @WebMethod(operationName = "getCadastreObject")
     public CadastreObjectTO getCadastreObject(@WebParam(name = "id") final String id)
             throws SOLAFault, UnhandledFault, SOLAAccessFault {
 
@@ -740,9 +836,8 @@ public class Cadastre extends AbstractWebService {
 
         return (CadastreObjectTO) result[0];
     }
-         
-         
-         /**
+
+    /**
      * See {@linkplain org.sola.services.ejb.cadastre.businesslogic.CadastreEJB#getSysRegWorkUnitByAllParts(java.lang.String)
      * CadastreEJB.getSysRegWorkUnitByAllParts}
      *
@@ -770,22 +865,20 @@ public class Cadastre extends AbstractWebService {
 
         return (SysRegWorkUnitTO) result[0];
     }
-   
 
-     /**
-     * See {{@linkplain CadastreEJB#saveSysRegWorkUnit(List<SysRegWorkUnitTO>, String)
-     * CadastreEJB.saveSysRegWorkUnit(byte[], Integer, Integer)}
-     * 
-     * @param items 
-     * @param languageCode 
-     * 
+    /**
+     * See {{@linkplain CadastreEJB#saveSysRegWorkUnit(List<SysRegWorkUnitTO>,
+     * String) CadastreEJB.saveSysRegWorkUnit(byte[], Integer, Integer)}
+     *
+     * @param items
+     * @param languageCode
+     *
      * @throws SOLAAccessFault
      * @throws SOLAFault
      * @throws UnhandledFault
      * @throws OptimisticLockingFault
      * @throws SOLAValidationFault
      */
-
     @WebMethod(operationName = "SaveSysRegWorkUnit")
     public SysRegWorkUnitTO SaveSysRegWorkUnit(
             @WebParam(name = "items") SysRegWorkUnitTO items,
@@ -806,11 +899,11 @@ public class Cadastre extends AbstractWebService {
 
                     SysRegWorkUnit newWork = cadastreEJB.saveSysRegWorkUnit(
                             GenericTranslator.fromTO(workunit, SysRegWorkUnit.class,
-                            cadastreEJB.getSysRegWorkUnitByIds(workunit.getId())), languageCodeTmp);
+                                    cadastreEJB.getSysRegWorkUnitByIds(workunit.getId())), languageCodeTmp);
                     result[0] = GenericTranslator.toTO(newWork, SysRegWorkUnitTO.class);
                 }
             }
-        });  
+        });
 //            @Override
 //            public void run() {
 //                
@@ -823,8 +916,8 @@ public class Cadastre extends AbstractWebService {
 //        });
         return (SysRegWorkUnitTO) result[0];
     }
-    
-      /**
+
+    /**
      * Uses the {@linkplain org.sola.services.ejb.system.businesslogic.SystemEJB#getCodeEntityList(java.lang.Class, java.lang.String)
      * SystemEJB.getCodeEntityList} to retrieve the BrTechnicalType codes.
      *
@@ -850,20 +943,19 @@ public class Cadastre extends AbstractWebService {
         return (List<LevelTO>) result[0];
     }
 
-     /**
+    /**
      * See {{@linkplain CadastreEJB#getSpatialUnits(byte[], String, Integer)
      * CadastreEJB.getSpatialUnits(byte[], String, Integer)}
-     * 
-     * @param filteringGeometry 
+     *
+     * @param filteringGeometry
      * @param levelId
-     * @param srid 
-     * 
+     * @param srid
+     *
      * @throws SOLAAccessFault
      * @throws SOLAFault
      * @throws UnhandledFault
      */
-
-     @WebMethod(operationName = "GetSpatialUnits")
+    @WebMethod(operationName = "GetSpatialUnits")
     public List<SpatialUnitTO> GetSpatialUnits(
             @WebParam(name = "filteringGeometry") final byte[] filteringGeometry,
             @WebParam(name = "levelId") final String levelId,
@@ -885,20 +977,19 @@ public class Cadastre extends AbstractWebService {
         return (List<SpatialUnitTO>) result[0];
     }
 
-     /**
-     * See {{@linkplain CadastreEJB#saveSpatialUnits(List<SpatialUnitTO>, String)
-     * CadastreEJB.saveSpatialUnits(byte[], Integer, Integer)}
-     * 
-     * @param items 
-     * @param languageCode 
-     * 
+    /**
+     * See {{@linkplain CadastreEJB#saveSpatialUnits(List<SpatialUnitTO>,
+     * String) CadastreEJB.saveSpatialUnits(byte[], Integer, Integer)}
+     *
+     * @param items
+     * @param languageCode
+     *
      * @throws SOLAAccessFault
      * @throws SOLAFault
      * @throws UnhandledFault
      * @throws OptimisticLockingFault
      * @throws SOLAValidationFault
      */
-
     @WebMethod(operationName = "SaveSpatialUnits")
     public void SaveSpatialUnits(
             @WebParam(name = "items") List<SpatialUnitTO> items,
@@ -914,16 +1005,15 @@ public class Cadastre extends AbstractWebService {
             @Override
             public void run() {
                 List<String> ids = new ArrayList<String>();
-                for(SpatialUnitTO item: itemsTmp){
+                for (SpatialUnitTO item : itemsTmp) {
                     ids.add(item.getId());
                 }
-                List<SpatialUnit> spatialUnitGroupListToSave =
-                        GenericTranslator.fromTOList(itemsTmp, SpatialUnit.class, 
-                        cadastreEJB.getSpatialUnitsByIds(ids));
+                List<SpatialUnit> spatialUnitGroupListToSave
+                        = GenericTranslator.fromTOList(itemsTmp, SpatialUnit.class,
+                                cadastreEJB.getSpatialUnitsByIds(ids));
                 cadastreEJB.saveSpatialUnits(spatialUnitGroupListToSave, languageCodeTmp);
             }
         });
     }
 
 }
-
